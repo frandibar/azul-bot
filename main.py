44de3169fe381a1e3b08b696a80729cf94e9b1dd
Francisco Dibar <francisco@xapo.com>
2015-09-19 01:28:30 -0300

    wip

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import os
+
 from flask import Flask
+from twx.botapi import TelegramBot
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CERT      = os.getenv("CERT")
+CERT_KEY  = os.getenv("CERT_KEY")
+HOST      = "azul-bot.herokuapp.com"
+PORT      = 8443
 
 app = Flask(__name__)
+context = (CERT, CERT_KEY)
 
-@app.route('/')
-def home():
-    return 'Hello World!'
+bot = TelegramBot(BOT_TOKEN)
+
+
+# This strange string is part of the bot's token
+# I use it so that only telegram hits it.
+@app.route("/" + BOT_TOKEN, methods=['POST'])
+def webhook():
+    # update = telegram.update.Update.de_json(request.get_json(force=True))
+    # bot.sendMessage(chat_id=update.message.chat_id, text='Hello, there')
+    reply = "hola desde heroku"
+    chat_id = 131602840
+    bot.send_message(chat_id, reply).wait()
+    return "OK"
+
+
+def set_webhook():
+    bot.set_webhook(webhook_url='https://%s:%s/%s' % (HOST, PORT, BOT_TOKEN),
+                    certificate=open(CERT, "rb"))
+
+
+if __name__ == '__main__':
+    set_webhook()
+
+    app.run(host='0.0.0.0',
+            port=PORT,
+            ssl_context=context,
+            debug=True)
