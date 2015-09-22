29bdd6ac27734e230eddfca13891101f01f19284
Francisco Dibar <francisco@xapo.com>
2015-09-22 00:33:18 -0300

    wip

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 
 import flask
@@ -11,18 +12,36 @@ app = flask.Flask(__name__)
 
 bot = telegram.Bot(BOT_TOKEN)
 
+CMD_START      = "/start"
+CMD_HELP       = "/help"
+CMD_ABOUT      = "/about"
+CMD_COTIZACION = "/cotizacion"
+
+USDARSB = "USD BLUE"
+USDARS  = "USD Oficial"
+BTCUSD  = "BTC USD"
+BTCARS  = "BTC ARS"
+
 
 # This strange string is part of the bot's token
 # I use it so that only telegram hits it.
 @app.route("/" + BOT_TOKEN, methods=["POST"])
 def webhook():
-    # reply = "hola desde heroku"
-    # chat_id = 131602840
-
     update = telegram.update.Update.de_json(flask.request.get_json(force=True))
-    ret = bot.sendMessage(chat_id=update.message.chat_id, text='Hello, loco')
+    if update.message.text.startswith(CMD_COTIZACION):
+        custom_keyboard = [[USDARSB, USDARS],
+                           [BTCUSD, BTCARS]]
+        reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard,
+                                                    one_time_keyboard=True)
+        return bot.sendMessage(chat_id=update.message.chat_id,
+                               text="Eleji una moneda.",
+                               reply_markup=reply_markup)
 
-    return "OK" if ret else "FAIL"
+    elif update.message.text.startswith(USDARSB):
+        return bot.sendMessage(chat_id=update.message.chat_id,
+                               text = "Ambito:  $XXX")
+    msg = "No entiendo. Escribime /help para que te entienda."
+    return bot.sendMessage(chat_id=update.message.chat_id, text=msg)
 
 
 @app.route("/remove_webhook")
